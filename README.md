0100003a7a37f0414c375a6edbceabb8c7b3070f
devgabrielvieira <gabriel_bardo@hotmail.com>
2023-08-17 22:30:34 -0300

    Projeto DIO v1.0

---
+++ README.md
@@ -0,0 +1,31 @@
+## 💰 Sistema Bancário em Python
+
+<p>O sistema permite que o usuário faça depósitos, saques e visualize seu extrato bancário.
+<br>O projeto foi desenvolvido como um exercício de programação e tem como objetivo demonstrar a lógica básica de um sistema bancário.</p>
+
+<h3>✔️ Funcionalidades</h3>
+
+<br><b><i>Depositar:</b></i> O usuário pode fazer depósitos na sua conta bancária.
+<br><b><i>Sacar:</b></i> O usuário pode fazer saques da sua conta bancária, desde que não ultrapasse o limite de saques e o valor disponível na conta.
+<br><b><i>Extrato:</b></i> O usuário pode visualizar o extrato da sua conta, incluindo todas as transações de depósito e saque realizadas.
+<br><b><i>Limite de Saques:</b></i> O sistema possui um limite máximo de saques por conta bancária.
+<br><b><i>Validação de Entradas:</b></i> O sistema realiza validações para garantir que as entradas do usuário sejam válidas.
+
+<h3>🗺️ Como Usar</h3>
+
+<br>Clone o repositório para o seu computador.
+<br>Navegue até o diretório do projeto.
+<br>Execute o arquivo sistema_bancario.py em um interpretador Python.
+<br>Siga as instruções exibidas no console para realizar as operações desejadas.
+
+<h3>📚 Contribuição</h3>
+
+<br>Contribuições são bem-vindas! Se você tiver alguma sugestão de melhoria ou encontrar algum bug, por favor, abra uma issue ou envie um pull request.
+
+<h3>👨🏽‍💻 Autor</h3>
+
+<br>Este projeto foi desenvolvido por Gabriel Vieira.
+
+<h3>📋 Licença</h3>
+
+<br>Este projeto está licenciado sob a Licença MIT.
---
+++ sistema_bancario.py
@@ -0,0 +1,71 @@
+menu = """
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+===> """
+
+saldo = 0
+limite = 500
+extrato = []
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+
+def depositar():
+    global saldo, extrato
+    valor_deposito = float(input("Digite o valor a ser depositado: "))
+
+    if valor_deposito >= 0:
+        saldo += valor_deposito
+        extrato.append(f"Depósito de R${valor_deposito:.2f}\n")
+        print(f"Depósito de R${valor_deposito:.2f} realizado com sucesso.")
+    else:
+        print("O valor do depósito não pode ser negativo.")
+
+
+def sacar():
+    global saldo, extrato, numero_saques
+    if numero_saques < LIMITE_SAQUES:
+        valor_saque = float(input("Digite o valor a ser sacado: "))
+
+        if valor_saque <= saldo and valor_saque <= limite and valor_saque >= 0:
+            saldo -= valor_saque
+            extrato.append(f"Saque de R${valor_saque:.2f}\n")
+            numero_saques += 1
+            print(f"Saque de R${valor_saque:.2f} realizado com sucesso. ")
+        else:
+            print(
+                "Saldo insuficiente, valor negativo ou valor de saque acima do limite."
+            )
+    else:
+        print("Limite de saques atingido.")
+
+
+def exibir_extrato():
+    print("\n===================== Extrato: =====================")
+    for transacao in extrato:
+        print(transacao)
+    print(f"\nSaldo atual: R${saldo:.2f}")
+    print(f"Limite de saques restantes: {LIMITE_SAQUES - numero_saques}")
+    print("\n======================================================")
+
+
+while True:
+    try:
+        opcao = int(input(menu))
+    except ValueError:
+        print("Opção inválida, por favor selecione novamente a operação desejada.")
+        continue
+
+    if opcao == 1:
+        depositar()
+    elif opcao == 2:
+        sacar()
+    elif opcao == 3:
+        exibir_extrato()
+    elif opcao == 0:
+        break
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
